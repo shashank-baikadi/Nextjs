12f632294ea19072d41a1f2c5953af68c9090c26
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-05-11 17:21:15 +0530

    chore: Update layout and remove unused code

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <p className="bg-red-200
+    ">Page</p>;
+  }
---
+++ nextjs-dashboard/app/dashboard/shashank/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>http://localhost:3000/dashboard/shashank</p>;
+  }
